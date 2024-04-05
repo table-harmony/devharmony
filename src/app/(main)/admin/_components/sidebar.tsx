50acf56341ab94b6b809fd60c22653a72068f192
liron1111 <lironkaner2007@gmail.com>
2024-04-05 18:50:03 +0300

    updating admin sidebar
    removing search for tables in admin page

--- src/app/(main)/admin/_components/sidebar.tsx
+++ src/app/(main)/admin/_components/sidebar.tsx
@@ -1,32 +1,10 @@
-"use client";
-
 import Link from "next/link";
-import { useCallback, useEffect, useState } from "react";
 import { DatabaseIcon, Table2Icon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-
-import { getModelsAction } from "../_actions/get-models.action";
+import { models } from "../models";
 
 export default function Sidebar() {
-  const [filterTable, setFilterTable] = useState("");
-  const [models, setModels] = useState([""]);
-
-  const onSubmit = useCallback(() => {
-    getModelsAction()
-      .then((data) => {
-        setModels(data);
-      })
-      .catch(() => {
-        console.log("error");
-      });
-  }, []);
-
-  useEffect(() => {
-    onSubmit();
-  }, [onSubmit]);
-
   return (
     <div className="flex flex-col gap-4">
       <Button variant="outline" className="justify-start" asChild>
@@ -35,24 +13,13 @@ export default function Sidebar() {
         </Link>
       </Button>
       <div className="flex flex-col gap-1">
-        <Input
-          placeholder="Search tables"
-          onChange={(event) => setFilterTable(event.target.value)}
-        />
-        {models
-          .filter((model) => model.includes(filterTable.toLowerCase()))
-          .map((model) => (
-            <Button
-              variant="ghost"
-              key={model}
-              className="justify-start"
-              asChild
-            >
-              <Link href={`/admin/${model}`}>
-                <Table2Icon className="w-4 h-4 mr-2" /> {model}
-              </Link>
-            </Button>
-          ))}
+        {Object.keys(models).map((model) => (
+          <Button key={model} variant="ghost" className="justify-start" asChild>
+            <Link href={`/admin/${model}`}>
+              <Table2Icon className="w-4 h-4 mr-2" /> {model}
+            </Link>
+          </Button>
+        ))}
       </div>
     </div>
   );
