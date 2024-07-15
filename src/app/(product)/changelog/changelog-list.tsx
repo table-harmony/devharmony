660f2713b25e71fc79a8f7163e99612f3f84c9f5
table-harmony <lironkaner2007@gmail.com>
2024-07-15 20:03:16 +0300

    Revert mistakes

--- src/app/(product)/changelog/changelog-list.tsx
+++ src/app/(product)/changelog/changelog-list.tsx
@@ -19,7 +19,7 @@ export async function ChangelogList() {
   unstable_noStore();
 
   const changelogs = await fetch(
-    `https://projectplannerai.com/api/changelog?projectId=j5701hxfwpmeqn625rjqgestb16qvhe0`,
+    `https://projectplannerai.com/api/changelog?projectId=${projectId}`,
   ).then(async (res) => res.json() as Promise<ChangeLog[]>);
 
   return (
